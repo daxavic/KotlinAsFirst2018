67722b6c892f974fe05e710c2522d61dc3653a98
Victorova Darya <daviktorova@mail.ru>
2018-10-23 18:19:27 +0300

    3 урок

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -150,6 +150,7 @@ fun maxDivisor(n: Int): Int = n / minDivisor(n)
  * Например, 25 и 49 взаимно простые, а 6 и 8 -- нет.
  */
 fun isCoPrime(m: Int, n: Int): Boolean {
+    if (m == 1 && n == 1) return true
     if (max(m, n) % min(m, n) == 0) return false
     for (i in 2..min(m, n) / 2) {
         if (m % i == 0 && n % i == 0) return false
