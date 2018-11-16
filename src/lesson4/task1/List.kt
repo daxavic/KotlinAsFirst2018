1b901f7d50f43228a711ffb60152cafce54b885f
Victorova Darya <daviktorova@mail.ru>
2018-11-16 21:49:28 +0300

    4 урок

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -244,11 +244,10 @@ fun convert(n: Int, base: Int): List<Int> {
  * Например: n = 100, base = 4 -> 1210, n = 250, base = 14 -> 13c
  */
 fun convertToString(n: Int, base: Int): String = convert(n, base).joinToString(
-        separator = "",
-        transform = {
-            if (it > 9) ('a' + it - 10).toString()
-            else "$it"
-        })
+        separator = "") {
+    if (it > 9) ('a' + it - 10).toString()
+    else "$it"
+}
 
 /**
  * Средняя
@@ -276,10 +275,12 @@ fun decimal(digits: List<Int>, base: Int): Int {
  * 10 -> a, 11 -> b, 12 -> c и так далее.
  * Например: str = "13c", base = 14 -> 250
  */
-fun decimalFromString(str: String, base: Int): Int = decimal(str.map {
-    if (it >= 'a') (it - 'a' + 10)
-    else it - '0'
-}, base)
+fun charToInt(i: Char): Int = when (i) {
+    in '0'..'9' -> i - '0'
+    else -> i - 'a' + 10
+}
+
+fun decimalFromString(str: String, base: Int): Int = decimal(str.map { charToInt(it) }, base)
 
 /**
  * Сложная
