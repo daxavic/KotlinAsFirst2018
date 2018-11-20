cd2861102360cd173aef7c59d8dfbf6e52927be0
Victorova Darya <daviktorova@mail.ru>
2018-11-21 02:35:11 +0300

    5 Lesson

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -251,7 +251,8 @@ fun whoAreInBoth(a: List<String>, b: List<String>): List<String> = a.toSet().int
  * Например:
  *   canBuildFrom(listOf('a', 'b', 'o'), "baobab") -> true
  */
-fun canBuildFrom(chars: List<Char>, word: String): Boolean = word.toLowerCase().toSet().all { chars.contains(it) }
+fun canBuildFrom(chars: List<Char>, word: String): Boolean =
+        word.toLowerCase().toSet().all { chars.joinToString(separator = "").toLowerCase().contains(it) }
 
 /**
  * Средняя
